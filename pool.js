1442b223dd2e66853559179aa748bd4684f1e3ae
Hero-Gamer727 <83517823+Hero-Gamer727@users.noreply.github.com>
2022-03-12 09:43:42 -0600

    Add files via upload

--- pool.js
+++ pool.js
@@ -1,40 +1,58 @@
-model_status="";
+model_status = "";
+objects = [];
+3
 
-function preload(){
-    img=loadImage("IMG_2228.jpg");
+function preload() {
+    img = loadImage("IMG_2228.jpg");
 }
 
 
 
-function setup(){
-    canvas=createCanvas(640,420);
+function setup() {
+    canvas = createCanvas(640, 420);
     canvas.center();
 
     object_detector = ml5.objectDetector("cocossd", modelLoaded);
-    document.getElementById("status").innerHTML="Detecting Objects";
+    document.getElementById("status").innerHTML = "Detecting Objects";
 }
 
-function modelLoaded(){
+function modelLoaded() {
     console.log("Model Loaded!");
-model_status=true;
-object_detector.detect(img,gotResults);
+    model_status = true;
+    object_detector.detect(img, gotResults);
 
 }
 
 
-function draw(){
-    image(img,0,0,640,420);
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (model_status != "") {
+        for (i=0; i<objects.length; i++) {
+            noFill();
+            stroke("red");
+            strokeWeight(2);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+fill("blue");
+stroke("blue");
+strokeWeight(1);
+text(objects[i].label, objects[i].x, objects[i].y-10);
+text(floor(objects[i].confidence * 100),objects[i].x+20, objects[i].y-10);
+
+
+        }
+    }
 }
 
 
 
 
-function gotResults(error,results){
-if(error){
-    console.error(error);
-    
-}else{
-    console.log(results);
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
 
-}
+    } else {
+        console.log(results);
+        objects = results;
+
+    }
 }
